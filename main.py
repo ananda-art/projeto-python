71e5036b6cf3c20392fd848d4c2c8424432f8ae0
ananda <ananda.trindade@ba.estudande.senai.br>
2026-06-30 10:33:12 -0300

    Adiciona validação de idade

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 print("Sistema de verificação de idade")
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
-if idade >= 18:
-    print(nome, "é maior de idade.")
+if idade < 0:
+    print("Idade inválida.")
 else:
-    print(nome, "é menor de idade.")
+    if idade >= 18:
+        print(nome, "é maior de idade.")
+    else:
+        print(nome, "é menor de idade.")
+  
+
