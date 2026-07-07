4db1013a246e655bf18ae970ee2d114e1788a3a2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-07-07 11:28:59 -0300

    Adiciona mensagens personalizadas

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 print("Sistema de verificação de idade")
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
+
+# Mensagens personalizadas baseadas na idade
 if idade < 0:
     print("Idade inválida.")
+elif idade >= 18:
+    print(f"Olá, {nome}! Você é maior de idade. Seja bem-vindo ao sistema!")
 else:
-    if idade >= 18:
-        print(nome, "é maior de idade.")
-    else:
-        print(nome, "é menor de idade.")
-  
+    print(f"Olá, {nome}! Você é menor de idade. Seu acesso foi restrito.")
 
